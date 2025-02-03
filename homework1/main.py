9db1308700fe7c64d74b7c5c77c3072566cfc847
LukaElizbarashvili <elizbarashvililuka3@gmail.com>
2025-02-03 17:21:49 +0400

    done

--- homework1/main.py
+++ homework1/main.py
@@ -1,5 +1,22 @@
 def main() -> None:
-    # Write your solution here
+    # Task One
+    first_name = "Luka"
+    last_name = "Elizbarashvili"
+    full_name = first_name + " " + last_name
+    print("I am",full_name)
+
+    # Task Two
+    age = 23
+    print('I Am', age ,'years old')
+
+    # Task Three
+    temperature = 20.3
+    print("The Temperature is", temperature)
+
+    # Task Four
+    temperature = 35
+    is_hot = temperature > 30
+    print("It is hot:", is_hot)
     pass
 
 
